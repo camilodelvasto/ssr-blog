d497c29cf4d76a962a09d78533e7fa6840a128de
Camilo Delvasto <camilodelvasto@gmail.com>
2017-08-23 11:59:20 -0500

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,16 @@
+const config = require('./.contentful.json')
+
 module.exports = {
+  /*
+  ** Contentful settings
+  */
+  env: {
+    CTF_SPACE_ID: config.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: config.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID
+  },
+
   /*
   ** Headers of the page
   */
